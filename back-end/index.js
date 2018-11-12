b5c1180112a4477b481572f2d08f714fca353af5
Nuri Amari <nuri.amari99@gmai.com>
2018-11-12 06:43:36 -0500

    Set up login and authentication endpoints

--- back-end/index.js
+++ back-end/index.js
@@ -3,10 +3,19 @@ const session = require('express-session');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
-const port = 4000;
 require('dotenv').config();
 
-mongoose.connect(process.env.DB_URL, {useNewUrlParser: true });
+// db models
+const User = require('./models/user');
+
+// connect to db
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true }, (error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("db connection established");
+    }
+});
 
 // create a list of domains to white list from cors protection
 const whitelist = ['file:///Users/nuriamari/Developer/Waterloop/Waterloop-Team-Manager/front-end/dist/index.html']
@@ -31,8 +40,41 @@ app.use(session({
 
 app.get('/name', (req, res) => res.send('Nuri'));
 app.post('/login', (req, res) => {
-
+    User.findOne({username: req.body.username, password: req.body.password }, (error, userData) => {
+        if (error) throw error;
+        res.setHeader(200, {'Content-Type': 'application/json'});
+        if (!user) {
+            var response = {
+                'authStatus': 'denied',
+                'message': 'no user found',
+            };
+            res.end(JSON.stringify(response));
+        } else {
+            var response = {
+                'authSatus': 'success',
+                'username': user.username,
+            }    
+            req.session.authenticated = true;
+            res.end(JSON.stringify(response));
+        }
+    });
 });
 
-app.listen(process.env.PORT, () => console.log(`Example app listening on port ${port}!`));
+app.get('/authCheck', (req, res) => {
+
+    res.setHeader(200, {'Content-Type': 'application/json'});
+    if (req.session.authenticated) {
+        var response = {
+            'authStatus': 'success',
+        };
+        res.end(JSON.stringify(response));
+    } else {
+        var response = {
+            'authStatus': 'denied',
+        };
+        res.end(JSON.stringify(response));
+    }
+});
+
+app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`));
 
